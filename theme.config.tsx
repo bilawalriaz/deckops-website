7f9d226dbab2c6c2506a6c7bf5bc09c27b63a0d9
Bilawal Riaz <bilawalriaz@M3.local>
2024-03-24 00:19:40 +0000

    first

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Deck/Ops</span>,
+  project: {
+    link: 'https://github.com/bilawalriaz',
+  },
+  chat: {
+    link: 'https://discord.com/bilawalriaz',
+  },
+  docsRepositoryBase: 'https://deckops.dev',
+  footer: {
+    text: '© 2024 Deck/Ops',
+  },
+}
+
+export default config
